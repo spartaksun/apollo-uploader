d89e886704198ba8f4b9026e90a6b1b8d048a3e6
Alexey Belyakovskiy <spartak22>
2019-07-20 15:41:21 +0300

    Fixed gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,16 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('default', function () {
+gulp.task('js', function () {
     return tsProject.src()
         .pipe(tsProject())
-        .dts.pipe(gulp.dest('lib'));
+        .js.pipe(gulp.dest('lib'))
 });
+
+gulp.task('dts', function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .dts.pipe(gulp.dest('lib'))
+});
+
+gulp.task('default', gulp.series('js', 'dts'));
